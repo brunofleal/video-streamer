3d38b017beba32ec905b75e5c087d3b20bb50b95
root <root@localhost.localdomain>
2022-07-18 00:55:57 +0000

    Fix videos not updating list on startup

--- client/src/pages/Videos.jsx
+++ client/src/pages/Videos.jsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Pagination from '@mui/material/Pagination';
 
 function getSliceInterval(currentPage, maxPage, videosLength) {
-    let start = Math.round(((currentPage-1) / maxPage) * videosLength);
+    let start = Math.round(((currentPage - 1) / maxPage) * videosLength);
     let end = Math.round((currentPage / maxPage) * videosLength);
 
-    return [start, end]
+    return [start, end];
 }
 
 function Videos() {
@@ -20,54 +20,69 @@ function Videos() {
         videos.map((video) => {
             video.creationTimestamp = new Date(video.creationTimestamp);
             return video;
-        })
-        let sortedVideos = videos.sort((a,b) => {
+        });
+        let sortedVideos = videos.sort((a, b) => {
             return b.creationTimestamp - a.creationTimestamp;
         });
 
         return sortedVideos;
-    }
+    };
 
-    useEffect(()=> {
+    useEffect(() => {
         function fetchData() {
-            console.log('fetching videos')
+            console.log('fetching videos');
             fetch('/api/videos')
-            .then((res) => res.json())
-            .then((data) => {
-                let videos = sortByDate(data.data);
-                setVideos(videos);
-                handlePageChange(1);
-            });
+                .then((res) => res.json())
+                .then((data) => {
+                    let videosRetrieved = sortByDate(data.data);
+                    if (videosRetrieved) {
+                        console.log(`fetched ${videosRetrieved.length} videos`);
+                    }
+                    setVideos(videosRetrieved);
+                });
         }
         fetchData();
-    }, [])
+    }, []);
 
-    function handlePageChange (currentPage) {
-        let sliceInt = getSliceInterval(currentPage, Math.round(videos.length/linksPerPage), videos.length);
+    useEffect(() => {
+        if (videos) {
+            handlePageChange(1);
+        }
+    }, [videos]);
+
+    function handlePageChange(currentPage) {
+        let sliceInt = getSliceInterval(currentPage, Math.round(videos.length / linksPerPage), videos.length);
         setSliceInterval(sliceInt);
     }
 
-    function getVideosSubset() {
+    function getVideosSubset(videos) {
         let subset = videos.slice(sliceInterval[0], sliceInterval[1]);
         return subset;
     }
 
     return (
-    <Stack direction="column" spacing={1} style={{backgroundColor:'gray', width:'60vw', height:'80vh', overflow:'auto'}}>
-        <Pagination count={Math.round(videos.length/linksPerPage)} color="primary" backgroundColor={"white"} onChange={(event, page) => handlePageChange(page)}/>
-      {
-        getVideosSubset().map( (file) => {
-            return <Link href={`/video/${file.id}`}
-                textAlign={"left"}
-                borderBottom={'solid black 3px'}
-                padding={'5px'}>
-                {`${file.path.slice(file.path.lastIndexOf('/') + 1)} | size ${file.size}`}
-                </Link>
-        })
-      }
-
-    </Stack>
-    )
+        <Stack
+            direction="column"
+            spacing={1}
+            style={{ backgroundColor: 'gray', width: '60vw', height: '80vh', overflow: 'auto' }}>
+            <Pagination
+                count={Math.round(videos.length / linksPerPage)}
+                color="primary"
+                onChange={(event, page) => handlePageChange(page)} />
+            {
+                getVideosSubset(videos).map((file) => {
+                    return <Link
+                        key={file.id}
+                        href={`/video/${file.id}`}
+                        textAlign={"left"}
+                        borderBottom={'solid black 3px'}
+                        padding={'5px'}>
+                        {`${file.path.slice(file.path.lastIndexOf('/') + 1)} | size ${file.size}`}
+                    </Link>;
+                })
+            }
+        </Stack>
+    );
 }
 
 export default Videos;
